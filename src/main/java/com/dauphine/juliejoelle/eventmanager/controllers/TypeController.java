b6c27a509862d8d6cb3a65a08400148d8dba280e
juliehuyen <juliehuyen@outlook.fr>
2024-06-15 13:24:30 +0200

    ✨ Implémentation TypeController

--- src/main/java/com/dauphine/juliejoelle/eventmanager/controllers/TypeController.java
+++ src/main/java/com/dauphine/juliejoelle/eventmanager/controllers/TypeController.java
@@ -1,10 +1,30 @@
 package com.dauphine.juliejoelle.eventmanager.controllers;
 
+import com.dauphine.juliejoelle.eventmanager.entities.Type;
+import com.dauphine.juliejoelle.eventmanager.services.TypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/types")
 public class TypeController {
-    //TODO juju
+
+    private final TypeService typeService;
+
+    public TypeController(TypeService typeService) {
+        this.typeService = typeService;
+    }
+
+    @Operation(summary = "To get all types")
+    @GetMapping("")
+    public ResponseEntity<List<Type>> getTypes() {
+        List<Type> types = typeService.getAllTypes();
+        return ResponseEntity.ok(types);
+    }
 }
