46a932be8e4087cf540931c2305003ea555c139d
Joelle13 <joelle.huyen13@hotmail.fr>
2024-06-20 15:55:44 +0200

    ✨ Add loader event feedback

--- src/app/components/event-feedback-list/event-feedback-list.component.ts
+++ src/app/components/event-feedback-list/event-feedback-list.component.ts
@@ -11,12 +11,15 @@ export class EventFeedbackListComponent {
   allEvents: GameEvent[] = [];
   activeButton: string = '';
   sortOrder: 'date-asc' | 'date-desc' | 'part-asc' | 'part-desc' = 'part-desc';
+  loading: boolean = true;
 
   constructor(private eventService: EventService) {}
 
   ngOnInit(): void {
+    this.loading = true;
     this.eventService.getEventsPassed().subscribe((events: GameEvent[]) => {
       this.allEvents = events;
+      this.loading = false;
     })
   }
 
@@ -31,24 +34,29 @@ export class EventFeedbackListComponent {
   }
 
   sortEvents(): void {
+    this.loading = true;
     if (this.sortOrder === 'date-asc') {
       this.eventService.getEventsPassedSortedByDate(true).subscribe((events: GameEvent[]) => {
         this.allEvents = events;
+        this.loading = false;
       });
     }
     if (this.sortOrder === 'date-desc') {
       this.eventService.getEventsPassedSortedByDate(false).subscribe((events: GameEvent[]) => {
         this.allEvents = events;
+        this.loading = false;
       });
     }
     if (this.sortOrder === 'part-asc') {
       this.eventService.getEventsPassedSortedByParticipantsCount(false).subscribe((events: GameEvent[]) => {
         this.allEvents = events;
+        this.loading = false;
       });
     }
     if (this.sortOrder === 'part-desc') {
       this.eventService.getEventsPassedSortedByParticipantsCount(true).subscribe((events: GameEvent[]) => {
         this.allEvents = events;
+        this.loading = false;
       });
     }
   }
